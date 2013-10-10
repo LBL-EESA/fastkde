5d2ba8f6ca4cb05fe02556a6fd2c64bd64d3a9cc
taobrien@25988439-7013-4793-9e09-1eab0753cc58 <taobrien@25988439-7013-4793-9e09-1eab0753cc58>
2013-10-10 22:36:20 +0000

    Finally verified that the mulidimensional fast ECF method works as expected.  Fixed numerous bugs in the process.  Added a fair amount of commentary.
    --HG--
    branch : multiDimensionalBernacchia
    extra : convert_revision : svn%3A25988439-7013-4793-9e09-1eab0753cc58/branches/multiDimensionalBernacchia%4038

--- empiricalCharacteristicFunction.py
+++ empiricalCharacteristicFunction.py
@@ -32,10 +32,46 @@ class ECF:
                 tpoints, \
                 dataAverage = None, \
                 dataStandardDeviation = None, \
+                doStoreConvolution = False, \
                 useFFTApproximation = True):
+    """
+    Calculates the empirical characteristic function of arbitrary sets of
+    variables.
 
-    #TODO: check for regularity
+    Uses either the direct Fourier transform or nuFFT method (described by
+    O'Brien et al. (2014, J. Roy. Stat. Soc. C) to calculate the Fourier transform
+    of the data to yield the ECF.
+
+    """
+
+    #Set whether we use the nuFFT approximation
+    self.useFFTApproximation = useFFTApproximation
+
+    #Get the data shape (nvariables,ndatapoints)
+    dshape = shape(inputData)
+    rank = len(dshape)
+    if(rank != 2):
+      raise ValueError,"inputData must be a rank-2 array of shape [nvariables,ndatapoints]"
+    #Extract the number of variables
+    self.nvariables = dshape[0]
+    #Extract the number of data points
+    self.ndatapoints = dshape[1]
+
+
+    #Set the frequency points
     self.tpoints = tpoints
+    #Check for regularity if we are doing nuFFT
+    if(self.useFFTApproximation):
+      #Get the spacing of the first two points
+      dt = tpoints[1]-tpoints[0]
+      #Get the spacing of all points
+      deltaT = tpoints[1:] - tpoints[:-1]
+      #Get the difference between these spacings
+      deltaTdiff = deltaT - dt
+      fTolerance = 1e-14
+      #Check that all these differences are 0
+      if(not all(abs(deltaTdiff < fTolerance))):
+        raise ValueError,"tpoints must be regularly spaced if useFFTApproximation is True"
 
     if(dataAverage != None):
       #TODO: Check for proper dimensioning
@@ -50,21 +86,23 @@ class ECF:
     else:
       self.dataStandardDeviation = std(inputData,1)
 
-    #Get the data shape (nvariables,ndatapoints)
-    #TODO: Check for proper dimensioning
-    dshape = shape(inputData)
-    self.nvariables = dshape[0]
-    self.ndatapoints = dshape[1]
 
+    #Set whether we store the convolved version
+    #of the data used in the FFT estimate; mainly
+    #used for diagnostic purposes
+    self.doStoreConvolution = doStoreConvolution
+    #Initialize the convolvedData member to nothing
+    self.convolvedData = None
 
-    self.useFFTApproximation = useFFTApproximation
 
 
     if(self.useFFTApproximation):
+      #Calculate the ECF using the fast method
       self.ECF = self.__calculateECFbyFFT__(  inputData,\
                                               self.dataAverage, \
                                               self.dataStandardDeviation)
     else:
+      #Calculate the ECF using the slow (direct, but exact) method
       self.ECF = self.__calculateECFDirect__( inputData,\
                                               self.dataAverage, \
                                               self.dataStandardDeviation)
@@ -81,14 +119,16 @@ class ECF:
 
     #Call a Fortran routine to do an efficient calculation of the density in
     #fourier space.
-
     ecf = ftn.calculateecfdirect( datapoints = mydata,  \
                                   dataaverage = myaverage, \
                                   datastd = mystddev, \
                                   tpoints = self.tpoints,  \
                                   freqspacesize = len(self.tpoints)**self.nvariables  \
                               )
+    #Reshape the flattened array into a proper multidimensional array
     ecf = reshape(ecf,self.nvariables*[len(tpoints)])
+
+
     return ecf
 
   #*****************************************************************************
@@ -100,7 +140,10 @@ class ECF:
     """Use the non-uniform FFT method to estimate the fourier representation of
     the input data."""
 
+    #Determine the points corresponding to the x-grid
     xpoints = calcXfromT(self.tpoints)
+    #Calculate the grid spacing
+    deltaX = xpoints[1] - xpoints[0]
 
     #Calculate details of the gaussian kernel
     tau = 1.5629
@@ -108,6 +151,16 @@ class ECF:
     nspreadhalf = nspread/2
     fourTau = 4*tau
 
+    if(self.nvariables > 1):
+      #If this is a multidimensional ECF, use meshgrid to create a multidimensional grid
+      #of all the frequency points
+      tpointgrids = asarray(meshgrid(*self.nvariables*[self.tpoints]))
+    else:
+      #Otherwise, just create a [1,len(tpoints)] array of the frequency points
+      tpointgrids = asarray([self.tpoints])
+
+    #Do a fast kernel density estimate, using the Greengard and Lee (2004, SIAM)
+    #kernel parameters
     kde = ftn.calculatekerneldensityestimate( datapoints = mydata,  \
                                               dataaverage = myaverage, \
                                               datastd = mystddev, \
@@ -117,70 +170,146 @@ class ECF:
                                               realspacesize = len(xpoints)**self.nvariables  \
                                             )
 
+    #Reshape the KDE estimate into a multidimensional array
     kde = reshape(kde,self.nvariables*[len(xpoints)])
+    if(self.doStoreConvolution):
+      self.convolvedData = kde
 
-    tmpfft = fft.ifft(fft.ifftshift(kde))
-    kdeFFT = tmpfft[:len(self.tpoints),:len(self.tpoints)]
-
-    tpointgrids = meshgrid(*self.nvariables*[self.tpoints])
+    #Take the FFT of the KDE estimate (ifftshift is used to reorder the kde
+    #array such that 0 is the lowest corner [first index] of the array, as
+    #required by ifft)
+    tmpfft = fft.ifftn(fft.ifftshift(kde))
+    #Generate a set of array slices to access the lower half of the array
+    firstHalfSlice = tuple(self.nvariables*[slice(0,len(tpoints))])
+    #Extract the lower half of the tmpfft hypercube, which corresponds
+    #to the >0 frequencies
+    kdeFFT = tmpfft[firstHalfSlice]
 
     #Deconvolve FFT(kde) (divide by the FFT of the gaussian) to obtain the ECF estimate
-    deltaX = xpoints[1] - xpoints[0]
-    ecf = kdeFFT*exp(tau*(sum((tpointgrids*deltaX**2),axis=0)))/kdeFFT[0,0]
+    ecf = kdeFFT*exp(tau*sum((tpointgrids*deltaX)**2,axis=0))/kdeFFT.ravel()[0]
 
     return ecf
 
 
 
 if(__name__ == "__main__"):
-  from mpl_toolkits.mplot3d import Axes3D
-  import matplotlib.pyplot as plt
 
 
+  #Set the random seed to 0 so the results are repetable
   random.seed(0)
 
+  #Flag whether to do the 1-D test
+  doOneDimensionalTest = False
+  if(doOneDimensionalTest):
+    import pylab as P
+    def mySTDGaus1D(x):
+      return 1./sqrt(2*pi) * exp(-x**2/2)
 
-  def mySTDGaus2D(x,y):
-    return 1./(2*pi) * exp(-(x**2 + y**2)/2)
+    #Set the real-space/frequency points (Hermitian FFT-friendly)
+    numXPoints = 513
+    xpoints = linspace(-20,20,numXPoints)
+    tpoints = calcTfromX(xpoints)
 
-  #Set the frequency points (Hermitian FFT-friendly)
-  numXPoints = 513
-  xpoints = linspace(-20,20,numXPoints)
-  deltaX = xpoints[1] - xpoints[0]
-  dum = fft.fftfreq(numXPoints,deltaX/(2*pi))
-  tpoints = dum[0:len(dum)/2 + 1]
-
-  xp2d,yp2d = meshgrid(xpoints,xpoints)
-  mygaus2d = mySTDGaus2D(xp2d,yp2d)
-  tmpcf = fft.ifft(fft.ifftshift(mygaus2d))
-  mygauscf = tmpcf[:len(tpoints),:len(tpoints)]
-  mygauscf /= mygauscf[0,0]
+    #Calculate the FFT of an actual gaussian; use
+    #this as the empirical characteristic function standard
+    mygaus1d = mySTDGaus1D(xpoints)
+    tmpcf = fft.ifftn(fft.ifftshift(mygaus1d))
+    mygauscf = tmpcf[:len(tpoints)]
+    mygauscf /= mygauscf[0]
 
 
-  ndatapoints = 2**10
-  nvariables = 2
-  xyrand = random.normal(loc=0.0,scale=1.0,size=[nvariables,ndatapoints])
+    #Set the number of data points
+    ndatapoints = 2**10
+    #Set the number of variables
+    nvariables = 1
+    #Randomly sample from a normal distribution
+    xyrand = random.normal(loc=0.0,scale=1.0,size=[nvariables,ndatapoints])
 
-  ecfFFT = ECF(xyrand,tpoints,useFFTApproximation=True).ECF
-  ecfDFT = ECF(xyrand,tpoints,useFFTApproximation=False).ECF
+    #Calculat the ECF using the fast method
+    ecfFFT = ECF(xyrand,tpoints,useFFTApproximation=True).ECF
+    #Calculat the ECF using the slow method
+    ecfDFT = ECF(xyrand,tpoints,useFFTApproximation=False).ECF
 
-  tp2d,wp2d = meshgrid(tpoints,tpoints)
+    #Print the 0-frequencies (should be 1 for all)
+    print ecfFFT[0],ecfDFT[0],mygauscf[0]
 
-  fig = plt.figure()
-  ax = fig.add_subplot(211,projection='3d')
+    P.subplot(111,xscale="log")
+    #Plot the magnitude of the fast and slow ECFs
+    #(these should overlap for all but the highest half of the frequencies)
+    P.plot(tpoints,abs(ecfFFT),'r-')
+    P.plot(tpoints,abs(ecfDFT),'b-')
+    #Plot the gaussian characteristic function standard
+    P.plot(tpoints,abs(mygauscf),'k-')
+    P.show()
+    
+  doTwoDimensionalTest = True #Flag whether to do 2D tests
+  if(doTwoDimensionalTest):
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    def mySTDGaus2D(x,y):
+      return 1./(2*pi) * exp(-(x**2 + y**2)/2)
 
-  ax.plot_wireframe(tp2d[::4,::4],wp2d[::4,::4],(abs(ecfFFT)**2)[::4,::4],color='r')
-  ax.plot_wireframe(tp2d[::4,::4],wp2d[::4,::4],(abs(ecfDFT)**2)[::4,::4],color='b')
+    #Set the frequency points (Hermitian FFT-friendly)
+    numXPoints = 513
+    xpoints = linspace(-20,20,numXPoints)
+    tpoints = calcTfromX(xpoints)
 
-  ax2 = fig.add_subplot(212,xscale="log",yscale="log")
+    #Calculate points from a 2D gaussian, and take their 2D FFT
+    #to estimate the characteristic function standard
+    xp2d,yp2d = meshgrid(xpoints,xpoints)
+    mygaus2d = mySTDGaus2D(xp2d,yp2d)
+    tmpcf = fft.ifftn(fft.ifftshift(mygaus2d))
+    mygauscf = tmpcf[:len(tpoints),:len(tpoints)]
+    mygauscf /= mygauscf[0,0]
 
-  errorK = average(abs(ecfFFT-ecfDFT),0)
 
-  print ecfFFT[0,0],ecfDFT[0,0],mygauscf[0,0]
-  ax2.plot(tpoints,abs(average(ecfFFT,0))**2,'r-')
-  ax2.plot(tpoints,abs(average(ecfDFT,0))**2,'b-')
-  ax2.plot(tpoints,abs(average(mygauscf,0))**2,'k-')
+    #Sample points from a gaussian distribution
+    ndatapoints = 2**10
+    nvariables = 2
+    xyrand = random.normal(loc=0.0,scale=1.0,size=[nvariables,ndatapoints])
 
-  plt.show()
+    #Calculate the ECF using the fast method
+    CecfFFT = ECF(xyrand,tpoints,useFFTApproximation=True)
+    ecfFFT = CecfFFT.ECF
+    #Calculate the ECF using the slow method
+    CecfDFT = ECF(xyrand,tpoints,useFFTApproximation=False)
+    ecfDFT = CecfDFT.ECF
+
+    #Use meshgrid to generate 2D arrays of the frequency points
+    tp2d,wp2d = meshgrid(tpoints,tpoints)
+
+    #Create a figure
+    fig = plt.figure()
+
+    #Create a 3D set of axes
+    ax = fig.add_subplot(221,projection='3d')
+
+    #ax.plot_wireframe(tp2d[::4,::4],wp2d[::4,::4],(abs(mygauscf)**2)[::4,::4],color='k')
+    #plot the fast and slow ECFs using a wireframe (they should overlap to the eye)
+    ax.plot_wireframe(tp2d[::4,::4],wp2d[::4,::4],(abs(ecfFFT)**2)[::4,::4],color='r')
+    ax.plot_wireframe(tp2d[::4,::4],wp2d[::4,::4],(abs(ecfDFT)**2)[::4,::4],color='b')
+
+    #Create a 2D set of axes
+    ax2 = fig.add_subplot(222,xscale="log",yscale="log")
+
+
+    #Print the normalization constants (should be 1)
+    print ecfFFT[0,0],ecfDFT[0,0],mygauscf[0,0]
+
+    #plot the magnitudes of the fast and slow ECFs along
+    #an aribtrary slice (they should overlap except in the high frequency range)
+    ax2.plot(tpoints,abs(ecfFFT[5,:]),'r-')
+    ax2.plot(tpoints,abs(ecfDFT[5,:]),'b-')
+    #Plot the magnitude of the gaussian characteristic function
+    ax2.plot(tpoints,abs(mygauscf[5,:]),'k-')
+
+    #Plot the average difference between the slow and fast ECFs
+    #(will be relatively high because I use a coarse X grid, so that
+    # the slow calculation will finish in my lifetime)
+    errorK = average(abs(ecfFFT-ecfDFT),0)
+    ax3 = fig.add_subplot(223,xscale="log",yscale="log")
+    ax3.plot(tpoints[:len(tpoints)/2],errorK[:len(tpoints)/2],'k-')
+
+    plt.show()
 
 
--- test_determineDimensionIndices.py
+++ test_determineDimensionIndices.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from numpy import *
-import ftnbp11helper as ftn
+import ftnecf as ftn
 
 print ftn.determineflattenedindex.__doc__
 dimsize = 4
 ndims = 3
 npoints = dimsize**ndims
 
-testVals = range(1,dimsize**ndims + 1)
+testVals = asarray(range(1,dimsize**ndims + 1))
 testArray = reshape(testVals,ndims*[dimsize])
 
 print testArray
@@ -17,7 +17,7 @@ for i in range(npoints):
   
   k = ftn.determineflattenedindex(idimcounters=funcInds[i,:],dimsize=dimsize)
 
-  funcInds[i,:] -= 1 
   
-  print funcInds[i,:] + 1, \
-        i+1,k,testArray[funcInds[i,0],funcInds[i,1],funcInds[i,2]]
+  indtuple = tuple(funcInds[i,:]-1)
+  print funcInds[i,:], \
+        i+1,k,testArray[indtuple]
