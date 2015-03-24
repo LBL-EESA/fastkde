d01a65dc7f5f25f4041cf5cf8fba15c1557e7210
Travis A. O'Brien <TAOBrien@lbl.gov>
2015-03-24 11:47:59 -0700

    Fixed build for cloud9

--- setup.py
+++ setup.py
@@ -84,7 +84,8 @@ cmdclass = {'build_ext': build_ext}
 
 extensions = [ \
     Extension("floodFillSearch",["floodFillSearch.pyx"],libraries=libs,library_dirs=lib_dirs,include_dirs=inc_dirs,runtime_library_dirs=lib_dirs),\
-    Extension("nufft",["nufft.pyx"],libraries=libs,library_dirs=lib_dirs,include_dirs=inc_dirs,runtime_library_dirs=lib_dirs,extra_compile_args=["-fopenmp"],extra_link_args=['-fopenmp']),\
+    Extension("nufft",["nufft.pyx"],libraries=libs,library_dirs=lib_dirs,include_dirs=inc_dirs,runtime_library_dirs=lib_dirs),\
+    #Extension("nufft",["nufft.pyx"],libraries=libs,library_dirs=lib_dirs,include_dirs=inc_dirs,runtime_library_dirs=lib_dirs,extra_compile_args=["-fopenmp"],extra_link_args=['-fopenmp']),\
                 ]
 
 setup(
