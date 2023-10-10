498b4556d0f8ee17cbea3206b642bf6624156872
Travis A. O'Brien <obrienta@iu.edu>
2023-10-10 16:50:28 -0400

    fix version error for editable mode

--- fastkde/__init__.py
+++ fastkde/__init__.py
@@ -1,1 +1,6 @@
-from .version import version as __version__
+try:
+    from .version import version as __version__
+except ModuleNotFoundError:
+    # if the user did `pip install -e .`, there will be no version.py file
+    # therefore indicate that the version is "editable"
+    __version__ = "editable"
